c1583453b278117ef325edfa8e86a990fe4b796d
Yoseph birhane <yoseph7091@gmail.com>
2025-05-29 04:05:19 -0700

    Add script to print message based on number of arguments

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2); // Skip first 2 default args
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
