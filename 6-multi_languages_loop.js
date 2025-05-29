2a3f9e0196948f644cb1ca8d78b10b40e9acdb48
Yoseph birhane <yoseph7091@gmail.com>
2025-05-29 04:44:15 -0700

    Add script to print multiple languages using loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Create an array of strings
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through the array and print each line
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
